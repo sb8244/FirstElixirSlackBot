228d0a4cd6f10b6595b6ccab253f374eecc13a2f
Stephen Bussey <sb8244@cs.ship.edu>
2017-07-01 21:33:11 -0400

    Remove unused code

--- lib/first_slack_bot.ex
+++ lib/first_slack_bot.ex
@@ -1,18 +1,2 @@
 defmodule FirstSlackBot do
-  @moduledoc """
-  Documentation for FirstSlackBot.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> FirstSlackBot.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/first_slack_bot_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule FirstSlackBotTest do
-  use ExUnit.Case
-  doctest FirstSlackBot
-
-  test "the truth" do
-    assert 1 + 1 == 2
-  end
-end
